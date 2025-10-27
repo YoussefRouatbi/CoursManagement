d5f71545acba20b498e481db6c02f8d672ee91d3
habib13564 <habibtrabelsie@gmail.com>
2025-10-27 23:07:39 +0100

    7awlt nzid shwyae login lken el liasons failed

--- classwork/src/App.svelte
+++ classwork/src/App.svelte
@@ -2,27 +2,37 @@
   import Header from "./lib/header.svelte";
   import Navbar from "./lib/navbar.svelte";
   import Main from "./lib/main.svelte";
+  import { svelte-spa-router} from "svelte-spa-router";
+  import Main from "./main.js";
 
+  if (localStorage.getItem("loggedIn") !== "true") {
+    navigate("/");
+  }
   let username = "Habib";
   let profileImg = "public/profile.png";
   let opned = true;
   let selected = "Algorithme";
-  function HandelSelect(item){
-    selected = item
+  function HandelSelect(item) {
+    selected = item;
   }
 
   function toggleNavbar() {
     opned = !opned;
   }
 </script>
-<div class="overflow-y-auto  min-h-screen flex flex-col overflow-hidden bg-linear-to-b from-slate-800 via-slate-900 to-slate-950">
-  <div class="z-40">  <Header {username} {profileImg} {opned} on:toggle={toggleNavbar} /></div>
+
+<div
+  class="overflow-y-auto min-h-screen flex flex-col overflow-hidden bg-linear-to-b from-slate-800 via-slate-900 to-slate-950"
+>
+  <div class="z-40">
+    <Header {username} {profileImg} {opned} on:toggle={toggleNavbar} />
+  </div>
   <div class="flex flex-1 overflow-hidden">
     {#if opned}
-      <Navbar {selected} onselect = {HandelSelect}/>
+      <Navbar {selected} onselect={HandelSelect} />
     {/if}
     <div class="flex flex-1 p-5">
-      <Main {selected}/>
+      <Main {selected} />
     </div>
   </div>
 </div>
--- classwork/src/main.js
+++ classwork/src/main.js
@@ -1,6 +1,13 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import Login from './lib/login.svelte',
+import MainPage from "./lib/main.svelte";
+
+export default {
+  "/": Login,
+  "/main": MainPage,
+};
 
 const app = mount(App, {
   target: document.getElementById('app'),
